39af57bcb7aeadf9cd68b85bd855525b1fd5a94f
David Green <dave@activelylazy.co.uk>
2016-01-28 22:14:06 +0100

    Adding multiple coins increments value

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -6,7 +6,20 @@
 
         public void AddCoin(string coin)
         {
-            Total = +1;
+            if (coin == "1")
+                Total += 1;
+            else if (coin == "2")
+                Total += 2;
+            else if (coin == "5")
+                Total += 5;
+            else if (coin == "10")
+                Total += 10;
+            else if (coin == "20")
+                Total += 20;
+            else if (coin == "50")
+                Total += 50;
+            else if (coin == "100")
+                Total += 100;
         }
     }
 }
--- VendingMachine/VendingMachineTest.cs
+++ VendingMachine/VendingMachineTest.cs
@@ -19,5 +19,24 @@ namespace VendingMachine
 
                 .Then(vendingMachine.Total, Is(AnInt.EqualTo(1)));
         }
+
+        [Test]
+        public void AddingMultipleCoinsIncrementsValue()
+        {
+            VendingMachine vendingMachine;
+
+            Scenario()
+                .Given(vendingMachine = new VendingMachine())
+
+                .When(() => vendingMachine.AddCoin("1"))
+                .When(() => vendingMachine.AddCoin("2"))
+                .When(() => vendingMachine.AddCoin("5"))
+                .When(() => vendingMachine.AddCoin("10"))
+                .When(() => vendingMachine.AddCoin("20"))
+                .When(() => vendingMachine.AddCoin("50"))
+                .When(() => vendingMachine.AddCoin("100"))
+
+                .Then(vendingMachine.Total, Is(AnInt.EqualTo(188)));
+        }
     }
 }
